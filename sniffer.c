c64775f68502e59751132987de0d36a3cc307bd2
whokilleddb <debjeetbanerjee06042001@gmail.com>
2021-02-28 03:33:46 +0530

    Dump Packet In Hex And Display Layer 2 Information

---
+++ sniffer.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <features.h>
+#include <linux/if_packet.h>
+#include <linux/if_ether.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <strings.h>
+#include <string.h>
+
+int CreateRawSocket(int protocol_to_sniff)
+{
+	int rawsocket;
+	if((rawsocket=socket(PF_PACKET, SOCK_RAW,htons(protocol_to_sniff)))==-1)
+	{
+		perror("Error Creating Socket");
+		exit(-2);
+	}
+	return rawsocket;
+}
+
+int BindRawSocketToInterface(char *device, int rawsock,int protocol)
+{
+	struct sockaddr_ll sll;
+	struct ifreq ifr; // Stores Information About The Interface To Sniff On
+
+	bzero(&sll,sizeof(sll));
+	bzero(&ifr,sizeof(ifr));
+
+/* struct ifreq {
+               char ifr_name[IFNAMSIZ]; // Interface Name
+               union {
+                   struct sockaddr ifr_addr;
+                   struct sockaddr ifr_dstaddr;
+                   struct sockaddr ifr_broadaddr;
+                   struct sockaddr ifr_netmask;
+                   struct sockaddr ifr_hwaddr;
+                   short           ifr_flags;
+                   int             ifr_ifindex;
+                   int             ifr_metric;
+                   int             ifr_mtu;
+                   struct ifmap    ifr_map;
+                   char            ifr_slave[IFNAMSIZ];
+                   char            ifr_newname[IFNAMSIZ];
+                   char           *ifr_data;
+               };
+           };
+
+*/
+	/* Copy Device Name To ifr */
+	strncpy((char *)ifr.ifr_name, device,IFNAMSIZ);
+
+	if((ioctl(rawsock,SIOCGIFINDEX,&ifr))==-1)//Gets us the interface number of our device
+	{
+		perror("[-] Error Getting Interface Name!\n");
+		exit(-3);
+	}
+
+	/* Bind Socket To Interface */
+	sll.sll_family=AF_PACKET;
+	sll.sll_ifindex=ifr.ifr_ifindex;
+	sll.sll_protocol=htons(protocol);
+
+	if (( bind(rawsock,( struct  sockaddr *)&sll, sizeof(sll) ))==-1 )
+	{
+		perror("[-] Cannot Bind Socket To Interface \n");
+		exit(-4);
+	}
+
+	return 0;
+}
+
+int PrintPacketInHex(unsigned char *packet,int len)
+{
+	unsigned char *p =packet;
+	printf("\n\n-----------------Packet Begins-----------------\n\n");
+	while(len --)
+	{
+		printf("%.2x",*p);
+		p++;
+	}
+
+	printf("\n\n------------------Packet Ends------------------\n");
+}
+
+int PrintInHex(char *mesg, unsigned char *p, int len)
+{
+	printf(mesg);
+	while(len--)
+	{
+		printf("%.2X ",*p);
+		p++;
+	}
+}
+
+int ParseEthernetHeader(unsigned char *packet,int len)
+{
+	struct ethhdr *ethernet_header;
+
+	if(len>sizeof(struct ethhdr))
+	{
+		ethernet_header=(struct ethhdr *)packet;
+		PrintInHex("[+] Destination MAC : ", ethernet_header-> h_dest,6);
+		printf("\n");
+
+		PrintInHex("[+] Source MAC : ", ethernet_header-> h_source,6);
+		printf("\n");
+
+		PrintInHex("[+] Protocol : ", (void *)&ethernet_header-> h_proto,2);
+		printf("\n");
+
+
+
+
+	}
+	/* First 6 Bytes Are Destination  MAC */
+}
+
+int main(int argc, char **argv)
+{
+
+	if(argc!=3)
+	{
+		perror("[-] Wrong Usage\n");
+		exit(-99);
+	}
+	int raw;
+	unsigned char packet_buffer[2048];
+	int len;
+	int packets_to_sniff;
+	struct sockaddr_ll packet_info;
+	int packet_info_size=sizeof(packet_info);
+
+	/* Create A Raw Socket */	
+	raw = CreateRawSocket(ETH_P_IP);
+
+	/* Bind Socket To An Interface */
+	BindRawSocketToInterface(argv[1],raw,ETH_P_IP);
+
+	/* Get Number Of Packets To Sniff From User */
+	packets_to_sniff=atoi(argv[2]);
+
+	/* Start Sniffing And Print Hex Values */
+	while(packets_to_sniff--)
+	{
+		if((len=recvfrom(raw,packet_buffer,2048,0,(struct sockaddr*)&packet_info, &packet_info_size)) == -1)
+		{
+			perror("[-] Function recvfrom() returned exit status -1\n");
+			exit(-1);
+		}
+		
+		else
+		{
+			/* Packet Received */
+			PrintPacketInHex(packet_buffer, len);
+			ParseEthernetHeader(packet_buffer, len);
+		}
+		
+	}
+
+	return 0;
+}
